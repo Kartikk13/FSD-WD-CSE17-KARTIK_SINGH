8f95be0220d7b7cd9e69d8266cd829ba915d25e0
Kartikk13 <kartik.singh13082005@gmail.com>
2025-11-08 23:40:07 +0530

    added email and phone no validation in auth.js

--- WebDev/auth.js
+++ WebDev/auth.js
@@ -1,17 +1,28 @@
 function validateForm() {
-  let x = document.forms["myForm"]["FirstName"].value;
-  let y = document.forms["myForm"]["LastName"].value;
-  let z = document.forms["myForm"]["Password"].value;
-  if (((!/^[A-Za-z]+$/.test(x)) || (!/^[A-Za-z]+$/.test(y)))) {
+  let FirstName = document.forms["myForm"]["FirstName"].value;
+  let LastName = document.forms["myForm"]["LastName"].value;
+  let Password = document.forms["myForm"]["Password"].value;
+  let Email = document.forms["myForm"]["Email"].value;
+  let Phone = document.forms["myForm"]["phone"].value;
+  if (((!/^[A-Za-z]+$/.test(FirstName)) || (!/^[A-Za-z]+$/.test(LastName)))) {
     alert("Name must have only characters");
     return false;
   }
-  if (((x.length + y.length) < 6)) {
+  if (((FirstName.length + LastName.length) < 6)) {
     alert("Name must have more than 6 characters");
     return false;
   }
-  if (((z.length) < 6)) {
+  if (((Password.length) < 6)) {
     alert("Password should not be less than 6 characters length");
+    return false;
+  }
+  let emailPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+  if (!emailPattern.test(Email)) {
+    alert("Invalid email! Please use format: name@domain.com");
+    return false;
+  }
+  if(Phone.length != 10){
+    alert("Phone number should contain 10 digits only");
     return false;
   }
 }
